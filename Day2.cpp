21a109eb0232dac25e64bcf43ed84751a8af30d3
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-08-23 15:20:01 +0530

    Day2-Problem-CipherSchools CP

---
+++ Day2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int findsum(int* arr, int n)
+{
+    int flag = 0;
+    for(int i=0; i<n; i++)
+    {
+        for(int k=n-1; k>i+1; k--)
+        {
+            for(int j=i+1; j<k; j++)
+            {
+                if(arr[i] + arr[j] + arr[k] == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+    }
+    if(flag) return 1;
+    else return 0;
+}
+int main()
+{
+    int n;
+    cout <<"Enter the number of elements in array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements";
+    for(int i=0; i<n; i++)
+    cin >> arr[i];
+    cout << findsum(arr, n);
+    
+}
