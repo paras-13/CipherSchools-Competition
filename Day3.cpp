73f435d30a7ae30108c471a27ac502deb36ce07c
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-09-14 00:02:38 +0530

    Day 3 Problem - CipherSchools - Competetion

--- Day3.cpp
+++ Day3.cpp
@@ -1,94 +1,68 @@
+// Day 3
 #include <iostream>
-#include <bits/stdc++.h>
-
 using namespace std;
-
 class Node
 {
 public:
     int data;
     Node *next;
+    Node *prev;
     Node(int x)
     {
-        data = x;
-        next = NULL;
+        data = x; next = NULL; prev = NULL;
     }
 };
-void append(Node *&head, int data, int n)
+void insertNode(Node *&head, int val)
 {
-
-    Node *temp1 = new Node(data);
-    temp1->next = NULL;
-    if (n == 1)
+    Node *newnode = new Node(val);
+    newnode->next = NULL;
+    Node *temp = head;
+    if (head == NULL)
     {
-        temp1->next = head;
-        head = temp1;
+        head = newnode; return;
     }
-    Node *temp2 = head;
-    temp1->next = temp2->next;
-    temp2->next = temp1;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newnode; return;
 }
-
-void Sort(Node *&head, int size)
-{
-    Node *temp;
-    int i, j, flag;
-
-    for (i = 0; i < size - 1; i++)
-    {
-
-        temp = head;
-        flag = 0;
-
-        for (j = 0; j < size - i - 1; j++)
-        {
-
-            Node *p1 = temp;
-            Node *p2 = p1->next;
-
-            if (p1->data > p2->data)
-            {
-                swap(p1, p2);
-                flag = 1;
-            }
-
-            temp = temp->next;
-        }
-        if (flag == 0)
-            break;
-    }
-}
-
-void printnodelist(Node *&head)
+void rotateNode(Node* &head)
 {
     Node *temp = head;
-    while (temp != NULL)
+    while (temp->next->next != NULL) temp = temp->next;
+    Node* t = temp;
+
+    temp = temp->next;
+    temp->prev = t;
+    temp->prev->next = NULL;
+    temp->prev = NULL;
+    temp->next = head;
+    head = temp;
+}
+void print(Node *&head)
+{
+    Node *temp = head;
+    while(temp !=NULL)
     {
-        cout << temp->data << endl;
+        cout << temp->data << " ";
         temp = temp->next;
     }
 }
-
 int main()
 {
-    int n;
-    cout << "Enter number of elements in linked list";
-    cin >> n;
-    int arr[n];
-
+    int n, val;
     Node *head = NULL;
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-        append(head, arr[i], n);
-    }
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter elements: ";
 
-    cout << "--" << head->next->data << endl;
-    cout << "before adding of newnode : " << endl;
-    printnodelist(head);
-    Sort(head, n);
-    cout << "after adding of newnode : " << endl;
-    printnodelist(head);
-    return 0;
+    while (n--)
+    {
+        cin >> val;
+        insertNode(head, val);
+    }
+    int key;
+    cout << "Enter key: "; cin >> key;
+    for(int i=0; i<key; i++)
+    rotateNode(head);
+    print(head);
 }
