eb7f090c4ad95d2642c734d31e2fedab54346418
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-11-06 21:40:59 +0530

    Day1 Problem 1 Solved @cipherschools - competition

---
+++ Day1_prob1.cpp
@@ -0,0 +1,124 @@
+// Day1 Problem 1
+#include<iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+void insertNode(Node* &head, int val)
+{
+    Node* newnode = new Node(val);
+    newnode->next = NULL;
+    if (head == NULL) {
+        head = newnode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newnode;
+    newnode->prev = temp;
+}
+
+void reverseNode(Node* &head, Node* &t, int ch, Node* &main, Node* &tail, Node* &temp)
+{
+    Node* curr = head;
+    Node* prev = NULL;
+    Node* next = NULL;
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    if (ch)
+    {
+        tail->next = temp;
+        tail = head;
+        head = t;
+        temp = head;
+    }
+    else
+    {
+        tail = head;
+        head = t;
+        temp = head;
+        main = prev;
+    }
+}
+
+void traverseNode(Node* &head, int *arr, int n)
+{
+    int ch = 0, ptr=0;
+    Node* temp = head;
+    Node* tail = NULL;
+    Node* main = NULL;
+    Node* t = NULL;
+    int count = 0;
+    while (temp!= NULL)
+    {
+        count++;
+        if (count == arr[ptr] && n>=arr[ptr])
+        {
+            t = temp->next;
+            temp->next = NULL;
+            reverseNode(head, t, ch , main, tail, temp);
+            ch=1; n-=arr[ptr]; ptr++;
+            count = 0;
+        }
+        else if(count == n && n<arr[ptr])
+        {
+            t = temp->next;
+            temp->next = NULL;
+            reverseNode(head, t, ch , main, tail, temp);
+            n-=arr[ptr]; ptr++;
+            count = 0;
+        }
+        else temp = temp->next;
+    }
+    if(count!=0 && n>0) tail->next = t;
+    head = main;
+}
+void print(Node* head)
+{
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    int n, m, val;
+    cout << "Enter number of elements to enter: "; cin >> n;
+    int loose = n;
+    cout << "Enter elements: ";
+    while (loose--)
+    {
+        cin >> val;
+        insertNode(head, val);
+    }
+    cout << "Enter number of elements to enter in Array: ";
+    cin >> m;
+    int arr[m];
+    cout << "Enter elements in array: ";
+    for(int i=0; i<m; i++) cin >> arr[i];
+    traverseNode(head, arr, n);
+    print(head);
+    return 0;
+}
