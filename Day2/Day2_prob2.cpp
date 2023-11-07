572fce32179b04d40c99dd469cb044ea955661c3
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-11-07 23:01:22 +0530

    Day2 Problem-2 Solved @cipherschools Competition 4.0

--- Day2/Day2_prob2.cpp
+++ Day2/Day2_prob2.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int HistoArea(int* arr, int n) {
-        int prev, next, area=0;
+        int left, right, area=0;
         for(int i=0;i<n;i++)
         {
-            int count=1; prev=i-1; next=i+1;
-            while(prev>=0 && arr[i]<=arr[prev]) { 
-                count++; prev--;
+            int count=1; left=i-1; right=i+1;
+            while(left>=0 && arr[i]<=arr[left]) { 
+                count++; left--;
             }
-            while(next<n && arr[i]<=arr[next]) {
-                count++; next++;
+            while(right<n && arr[i]<=arr[right]) {
+                count++; right++;
             }
             int t_area=arr[i]*count;
             area=max(area, t_area);
